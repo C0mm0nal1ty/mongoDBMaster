37ff9cf8572bcdf5f4c458b88ab037bbab0fdd51
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-02-21 20:49:42 -0800

    more on mocha framework described and added in the doc file.

---
+++ users/test/create_test.js
@@ -0,0 +1,14 @@
+//This is to write some test on the user model. Testing the schema.
+//First, describe how you will test the data to Mocha.
+describe('Creating records', () => {
+  it('saves a user', () => {
+    //run an assertion: validate data
+    
+  });
+
+});
+
+
+
+
+//EOL
