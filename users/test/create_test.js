d380a656d41cb475ec3a69439c3c11eb108b3015
Noe Alvarado <44933742+CeroDosUno@users.noreply.github.com>
2019-02-21 21:07:21 -0800

    created first instanceand tested it with mocha.

--- users/test/create_test.js
+++ users/test/create_test.js
@@ -1,10 +1,20 @@
 //This is to write some test on the user model. Testing the schema.
 //First, describe how you will test the data to Mocha.
+//Assertion library
+const assert = require('assert');
+
+const User = require ('../src/user');
+
 describe('Creating records', () => {
   it('saves a user', () => {
     //run an assertion: validate data
-    
+    const joe = new User({ name: 'Joe' });
+
+    joe.save();
+
+
   });
+
 
 });
 
